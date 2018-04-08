0c09f039868fc02784bd73f6d2241e96dfbe3aa4
Andrew Turkin <andrew@Andrews-iMac.local>
2018-04-09 00:28:42 +0200

    Added MVVM design

---
+++ UIPatterns/MVVM/DataBinder.swift
@@ -0,0 +1,15 @@
+//
+//  DataBinder.swift
+//  DesignLibrary
+//
+//  Created by Andrew Turkin on 08.04.18.
+//  Copyright © 2018 Andrew Trukin. All rights reserved.
+//
+
+protocol DataBinder: class {
+    // define data which is accessible from View(Controller)
+}
+
+class WritingDataBinder: DataBinder {
+    // define data which is accessible from ViewModel
+}
---
+++ UIPatterns/MVVM/DefaultViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultViewModel.swift
+//  DesignLibrary
+//
+//  Created by Andrew Turkin on 08.04.18.
+//  Copyright © 2018 Andrew Trukin. All rights reserved.
+//
+
+class ConcreteViewModel: ViewModel {
+    var dataBinder: DataBinder = WritingDataBinder()
+    var writingDataBinder: WritingDataBinder { get{ return dataBinder as! WritingDataBinder } }
+    weak var delegate: ViewModelDelegate?
+}
